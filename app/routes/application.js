92dce285245614fb3088632bd97bf3d8f947841a
Dylan Jenkinson <dylan@dylan-jenkinson.nz>
2016-10-13 21:25:02 +1300

    Working on basic login page.

---
+++ app/routes/application.js
@@ -0,0 +1,4 @@
+import Ember from 'ember';
+import ApplicationRouteMixin from 'ember-simple-auth/mixin/application-route-mixin';
+
+export default Ember.Route.extend(ApplicationRouteMixin);
--- app/routes/login.js
+++ app/routes/login.js
@@ -1,13 +1,9 @@
 import Ember from 'ember';
 import config from './config/environment';
 
-const Router = Ember.Router.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
-});
+Router = Ember.Route.extend({});
 
 Router.map(function() {
-  this.route('about');
   this.route('login');
 });
 
