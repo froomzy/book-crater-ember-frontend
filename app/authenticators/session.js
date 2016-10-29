ccc55790015e215f842fa3db06129be1723796b1
Dylan Jenkinson <dylan.jenkinson@xtra.co.nz>
2016-10-30 09:19:50 +1300

    Made the login work as I want it to with the session and stuff.

--- app/authenticators/session.js
+++ app/authenticators/session.js
@@ -1,24 +1,38 @@
 import Ember from "ember";
 import Base from "ember-simple-auth/authenticators/base";
 import ENV from "book-crater-ember-frontend/config/environment";
-const { RSVP, isEmpty, run, computed } = Ember;
+const {RSVP, isEmpty, run, computed} = Ember;
 
 export default Base.extend({
   cookies: Ember.inject.service(),
   serverTokenEndpoint: '/api/core/sessions/',
 
   // TODO (Dylan): Add in a restore method. It will look to see if it can find the session token, and make sure it is valid.
+  restore(data) {
+    return new RSVP.Promise((resolve, reject) => {
+      const serverTokenEndpoint = this.get('serverTokenEndpoint');
+      const url = ENV.APP.host + serverTokenEndpoint;
+      this.makeRequest(url, {type: 'GET', xhrFields: {withCredentials: true}})
+        .done((data) => {
+          run(() => {
+            resolve(data);
+          });
+        }).fail((xhr) => {
+        run(null, reject, xhr.responseJSON || xhr.responseText);
+      });
+    });
+  },
   authenticate(email, password) {
     return new RSVP.Promise((resolve, reject) => {
-      const data                = { email, password };
+      const data = {email, password};
       const serverTokenEndpoint = this.get('serverTokenEndpoint');
-      const url                 = ENV.APP.host + serverTokenEndpoint;
+      const url = ENV.APP.host + serverTokenEndpoint;
       this.makeRequest(url, {type: 'POST', data: data, dataType: 'json', xhrFields: {withCredentials: true}})
         .done((data) => {
-        run(() =>{
-          resolve(data);
-        });
-      }).fail((xhr) => {
+          run(() => {
+            resolve(data);
+          });
+        }).fail((xhr) => {
         run(null, reject, xhr.responseJSON || xhr.responseText);
       });
     });
@@ -26,16 +40,15 @@ export default Base.extend({
   invalidate(data) {
     let cookieService = this.get('cookies');
     return new RSVP.Promise((resolve, reject) => {
-      const csrfToken           = cookieService.read('book-crater-django');
-      console.log(csrfToken);
+      const csrfToken = cookieService.read('book-crater-django');
       const serverTokenEndpoint = this.get('serverTokenEndpoint');
-      const url                 = ENV.APP.host + serverTokenEndpoint;
+      const url = ENV.APP.host + serverTokenEndpoint;
       this.makeRequest(url, {type: 'DELETE', xhrFields: {withCredentials: true}, headers: {'x-csrftoken': csrfToken}})
-      .done((data) => {
-        run(() =>{
-          resolve(data);
-        });
-      }).fail((xhr) => {
+        .done((data) => {
+          run(() => {
+            resolve(data);
+          });
+        }).fail((xhr) => {
         run(null, reject, xhr.responseJSON || xhr.responseText);
       });
     });
